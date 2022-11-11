39745307ca9993c0fce4a465fe8a1c982c12e4ed
Chhavi Sharma <chhavi.sharma@tothenew.com>
2022-11-11 18:38:25 +0530

    initial commit

---
+++ example/main.tf
@@ -0,0 +1,12 @@
+module "vpn" {
+    source                    = "git::https://github.com/tothenew/terraform-aws-vpn.git"
+    key_name                  = "tothenew"
+    iam_instance_profile      = "tothenew"
+    security_groups           = ["sg-999999999999"]
+    subnet_id                 = "subnet-999999999999"
+    project_name_prefix       = "dev-tothenew"
+    common_tags               = {
+      "Project"     = "ToTheNew",
+      "Environment" = "dev"
+    }
+}
---
+++ main.tf
@@ -0,0 +1,48 @@
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+output "test" {
+  value = data.aws_ami.ubuntu
+}
+
+data "template_file" "user_data" {
+    template = file("${path.module}/user_data.sh")
+}
+
+resource "aws_instance" "ec2" {
+    ami                     = data.aws_ami.ubuntu.id
+    instance_type           = var.instance_type
+    subnet_id               = var.subnet_id
+    vpc_security_group_ids  = var.security_groups
+    key_name                = var.key_name
+    iam_instance_profile    = var.iam_instance_profile
+    ebs_optimized           = var.ebs_optimized
+    disable_api_termination = var.disable_api_termination
+    disable_api_stop        = var.disable_api_stop
+    user_data_base64        = base64encode(data.template_file.user_data.rendered)
+    source_dest_check       = var.source_dest_check
+
+    volume_tags = merge(var.common_tags, tomap({ "Name" : "${var.project_name_prefix}-jenkins" }))
+    tags        = merge(var.common_tags, tomap({ "Name" : "${var.project_name_prefix}-jenkins" }))
+
+    root_block_device {
+      delete_on_termination = var.delete_on_termination
+      encrypted             = var.encrypted
+      volume_size           = var.root_volume_size
+      volume_type           = var.volume_type
+    }
+}
