445acc00d2efdd685cac64b88b7c70a2c902fe74
Lekhika G0swami <lekhika.goswami@tothenew.com>
2022-12-08 14:34:07 +0530

    merged files

--- example/main.tf
+++ example/main.tf
@@ -1,12 +1,18 @@
 module "vpn" {
-    source                    = "../"
-    key_name                  = var.key_name
-    security_groups           = var.security_groups
-    iam_instance_profile      = var.iam_instance_profile
-    subnet_id                 = var.subnet_id
-    project_name_prefix       = "dev-tothenew"
-    common_tags               = {
-      "Project"     = "ToTheNew",
-      "Environment" = "dev"
-    }
+  source                  = "../"
+  key_name                = var.key_name
+  security_groups         = var.security_groups
+  iam_instance_profile    = var.iam_instance_profile
+  subnet_id               = var.subnet_id
+  project_name_prefix     = "dev-tothenew"
+  ebs_optimized           = var.ebs_optimized
+  disable_api_termination = var.disable_api_termination
+  delete_on_termination   = var.delete_on_termination
+  encrypted               = var.encrypted
+  volume_size             = var.root_volume_size
+  volume_type             = var.volume_type
+  common_tags = {
+    "Project"     = "ToTheNew",
+    "Environment" = "dev"
+  }
 }
