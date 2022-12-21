0d0effd307216a51975c5c28aec822ae8716166a
Chhavi Sharma <60739490+ChhaviSharma-97@users.noreply.github.com>
2022-12-21 19:03:35 +0530

    Updated sg.tf

--- sg.tf
+++ sg.tf
@@ -22,9 +22,9 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port   = -1
+    from_port   = 0
     protocol    = "all"
-    to_port     = -1
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
