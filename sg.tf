8026a56b462ee271efda2543e2c22d20b5f4506a
Chhavi Sharma <60739490+ChhaviSharma-97@users.noreply.github.com>
2022-12-21 18:52:32 +0530

    Updated sg.tf

--- sg.tf
+++ sg.tf
@@ -5,13 +5,13 @@ resource "aws_security_group" "default" {
   vpc_id      = var.vpc_id
   ingress {
     from_port   = 80
-    protocol    = "-1"
+    protocol    = "tcp"
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 443
-    protocol    = "-1"
+    protocol    = "tcp"
     to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
